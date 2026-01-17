f13b9605dd733f51504f893c63c8401c97c6aafa
Thaddaeus Markle <tmarklebw88@gmail.com>
2026-01-17 16:37:38 -0600

    Add docstring for _p suffix

--- src/strategies.h
+++ src/strategies.h
@@ -6,6 +6,7 @@
 // Checks if there is any square with only one empty cell.
 // If so, fills in that last cell and returns true.
 // If not, returns false.
+// The *_p version sets *pos to the position that was changed, if any.
 bool full_house(Board board);
 bool full_house_p(Board board, size_t *pos);
 
